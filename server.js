2f82ef1bf820c23f584a53e35dafde8cc06342ff
DilanRaza1 <dilan.raza@iths.se>
2025-01-20 11:48:05 +0100

    added connection to mongodb

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+app.get("/", (req, res) => {
+  res.send("welcome to my mongoapi");
+});
+
+app.post("/blogspot", (req, res) => {
+  res.send("this is a postrequest");
+});
+
+mongoose.
+connect("mongodb+srv://dilan1raza:12345678910@dilanapi.jqdso.mongodb.net/Products_API?retryWrites=true&w=majority&appName=DilanAPI")
+.then(() => {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+    console.log("connected to mongo");
+  });
+});
